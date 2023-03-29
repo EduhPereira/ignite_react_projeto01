f0d3732530e5ea8b660ec42ee9ecc044d270fb5b
Eduardo Pereira <vital@MacBook-Pro-de-Vital.local>
2023-03-28 23:30:49 -0300

    style: header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,12 @@
 import styles from "./Header.module.css"
 
+import igniteLogo from "../../assets/ignite-logo.png"
+
 export function Header() {
     return (
-        <header className={styles.header}>Ignite Feed</header>
+        <header className={styles.header}>
+            <img src={igniteLogo} alt="Ignite Logo" />
+            <span>ignite feed</span>
+        </header>
     )
 }

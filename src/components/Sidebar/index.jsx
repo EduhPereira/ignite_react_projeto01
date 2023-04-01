445575a1cbd2a6cb9a2e43d57195d4673494eba4
Eduardo Pereira <vital@MacBook-Pro-de-Vital.local>
2023-03-31 22:56:54 -0300

    style: sidebar profile image n footer

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,4 +1,5 @@
 import style from "./style.module.css"
+import { PencilLine } from "phosphor-react"
 
 export function Sidebar() {
     return(
@@ -8,11 +9,17 @@ export function Sidebar() {
                 src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
             />
             <div className={style.profile}>
+                <img 
+                    src="https://github.com/EduhPereira.png" 
+                />
                 <strong>Eduardo Pereira</strong>
-                <span>Web Developer</span>
+                <span>Software Engineer</span>
             </div>
             <footer>
-                <a href="#">Editar Perfil</a>
+                <a href="#">
+                    <PencilLine size={20} />
+                    Editar Perfil
+                </a>
             </footer>
         </aside>
     )
